0c498b725314c081d342595edf4dcf276498b91a
zimkjh <zimkjh@naver.com>
2020-10-31 17:43:48 +0900

    DataStore cs file 생성

---
+++ Assets/Scripts/DataStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+    private int muscleCount = 0;
+
+    void Awake() {
+        muscleCount = PlayerPrefs.GetInt("muscleCount");
+    }
+
+    public void incMuscleCount(int incNum){
+        muscleCount += incNum;
+        setMuscleCount(muscleCount);
+        
+    }
+
+    public void setMuscleCount(int muscleCount){
+        PlayerPrefs.SetInt("muscleCount", muscleCount);
+    }
+    public int getMuscleCount(){
+        return muscleCount;
+    }
+    
+
+}
