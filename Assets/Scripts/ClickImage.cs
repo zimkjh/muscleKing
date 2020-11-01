44e58a7bae0b93d38a1389b464000a2ada62fe92
zimkjh <zimkjh@naver.com>
2020-11-01 17:28:55 +0900

    Image touch로 health 버는 방식 추가
    - 살찐 이미지 추가
    - 왼쪽 : button (유지), 오른쪽 : image (추가)

---
+++ Assets/Scripts/ClickImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickImage : MonoBehaviour, IPointerClickHandler
+{
+    public DataController dataController;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        dataController.incHealth(1);
+    }
+}
