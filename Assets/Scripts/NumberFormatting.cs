50934e499fc97af18ae4fe084fd62e3c2f0eba90
kpsy20 <kpsy20@naver.com>
2020-11-07 12:50:02 +0900

    number formatting완료, 이슈 발생
    int 범위가 -2,147,483,648 ~ 2,147,483,647 이런데 게임 10분만해도 충분히 넘어감. 문제 해결필요.

--- Assets/Scripts/NumberFormatting.cs
+++ Assets/Scripts/NumberFormatting.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 using System;
 public class NumberFormatting : MonoBehaviour
 {
-    string[] numbering = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA" };
+    string[] numbering = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
     public string formatting(int num)
     {
-        int order = 1;
+        
+        int order = 1000;
         int index = -1;
-        while (num >= order * 1000 & num < order * 1000000)
+        int temp = num;
+        while(temp >= 1000)
         {
-            order = order * 1000;
+            temp = temp / order;
             index = index + 1;
         }
-        float result = (float)Math.Round((float)num / (float)order, 1);
+        float result = (float)Math.Round((float)num / (float)(Math.Pow(order, (index+1))), 1);
         string result_string = num.ToString();
-        if (index != -1)
+        if (index != -1 & index <= 25)
         {
             result_string = result.ToString() + numbering[index];
+        }
+        if(index >= 26) //26 AA 27 AB ... 52 BA ... 78 CA
+        {
+            int first = (index / 26) -1;
+            int second = index - ((first+1)*26);
+            result_string = result.ToString() + numbering[first] + numbering[second];
         }
         return result_string;
     }
